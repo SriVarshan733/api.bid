c9d608684ace0bb22aedfb4cd5d5d4edadcdeaa4
SriVarshan733 <srivarshan.it20@bitsathy.ac.in>
2023-09-20 08:38:14 +0530

    efficient code

--- insight.py
+++ insight.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 import schedule
 import time
 
-def scrape_and_save_data():
-    url = "https://market.todaypricerates.com/Tamil-Nadu-vegetables-price"
-
+def scrape_and_save_data(url, output_file):
     # Send an HTTP GET request to the URL
     response = requests.get(url)
 
@@ -17,7 +15,7 @@ def scrape_and_save_data():
         table = soup.find("div", class_="Table")
 
         # Initialize an empty list to store the scraped data
-        vegetable_data = []
+        data = []
 
         # Find all rows in the table
         rows = table.find_all("div", class_="Row")
@@ -26,81 +24,41 @@ def scrape_and_save_data():
             cells = row.find_all("div", class_="Cell")
 
             # Extract data from each cell
-            vegetable_name = cells[0].text.strip()
+            name = cells[0].text.strip()
             unit = cells[1].text.strip()
             market_price = cells[2].text.strip()
             retail_price = cells[3].text.strip()
             shopping_mall = cells[4].text.strip()
 
             # Create a dictionary for the current row
-            vegetable_info = {
-                "Vegetable Name": vegetable_name,
+            item_info = {
+                "Name": name,
                 "Unit": unit,
-                "Market price": market_price,
+                "Market Price": market_price,
                 "Retail Price": retail_price,
                 "Shopping Mall": shopping_mall,
             }
 
             # Append the dictionary to the list
-            vegetable_data.append(vegetable_info)
+            data.append(item_info)
 
         # Save the scraped data to a JSON file
-        with open("vegetable_data.json", "w", encoding="utf-8") as json_file:
-            json.dump(vegetable_data, json_file, ensure_ascii=False, indent=4)
+        with open(output_file, "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
 
-        print("Data scraped and saved to vegetable_data.json")
+        print(f"Data scraped and saved to {output_file}")
     else:
-        print("Failed to retrieve the webpage.")
+        print(f"Failed to retrieve the webpage for {url}")
 
-    url = "https://market.todaypricerates.com/Tamil-Nadu-fruits-price"
+# Define URLs and output file names
+vegetables_url = "https://market.todaypricerates.com/Tamil-Nadu-vegetables-price"
+fruits_url = "https://market.todaypricerates.com/Tamil-Nadu-fruits-price"
+vegetables_output_file = "vegetable_data.json"
+fruits_output_file = "fruits_data.json"
 
-    # Send an HTTP GET request to the URL
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        # Find the table containing the data
-        table = soup.find("div", class_="Table")
-
-        # Initialize an empty list to store the scraped data
-        fruits_data = []
-
-        # Find all rows in the table
-        rows = table.find_all("div", class_="Row")
-
-        for row in rows:
-            cells = row.find_all("div", class_="Cell")
-
-            # Extract data from each cell
-            fruits_name = cells[0].text.strip()
-            unit = cells[1].text.strip()
-            market_price = cells[2].text.strip()
-            retail_price = cells[3].text.strip()
-            shopping_mall = cells[4].text.strip()
-
-            # Create a dictionary for the current row
-            fruits_info = {
-                "Fruits Name": fruits_name,
-                "Unit": unit,
-                "Market price": market_price,
-                "Retail Price": retail_price,
-                "Shopping Mall": shopping_mall,
-            }
-
-            # Append the dictionary to the list
-            fruits_data.append(fruits_info)
-
-        # Save the scraped data to a JSON file
-        with open("Fruits_data.json", "w", encoding="utf-8") as json_file:
-            json.dump(fruits_data, json_file, ensure_ascii=False, indent=4)
-
-        print("Data scraped and saved to Fruits_data.json")
-    else:
-        print("Failed to retrieve the webpage.")
-
-# Schedule the scraping function to run every minute once
-schedule.every(1).minutes.do(scrape_and_save_data)
+# Schedule the scraping functions to run every minute
+schedule.every(1).minutes.do(scrape_and_save_data, vegetables_url, vegetables_output_file)
+schedule.every(1).minutes.do(scrape_and_save_data, fruits_url, fruits_output_file)
 
 # Run the scheduling loop
 while True:
